be7c5e9d4cc55e472fcfa18feef8a9cb47bc5a78
slim8020 <slim8020@naver.com>
2021-08-02 17:17:06 +0900

    Add httpServer

---
+++ httpServer/server1.js
@@ -0,0 +1,9 @@
+const http = require('http');
+
+http.createServer((req, res) =>{
+    res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+    res.write('<h1>Hello Node!</h1>');
+    res.end('<p>Hello Server!</p>');
+}).listen(8081, ()=>{
+    console.log('8081번 포트에서 서버 대기중입니다!');
+});
